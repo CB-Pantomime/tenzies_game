9bfafb0c1f96086cfa594d727e323b7109009abf
CB-Pantomime <christopherbrown.contact@gmail.com>
2022-06-03 15:18:53 -0700

    diceElements printing to page from map method bringing in Die component and random number for each die

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
-
+import React, { useState } from 'react'
 import './style.css';
 import Die from './Die'
 
 function App() {
-  return (
 
-    function allNewDice() {
-      let newArray = []
-      for(let i = 0; i < 10; i++){
-        let randomNum = Math.floor(Math.random() * 6 +1);
-        newArray.push(randomNum)
-      }
-      console.log(newArray)
+  const [dice, setDice] = React.useState(allNewDice())
+
+  
+  function allNewDice() {
+    let newArray = []
+    for(let i = 0; i < 10; i++){
+      let randomNum = Math.floor(Math.random() * 6 +1);
+      newArray.push(randomNum)
     }
-   
+    return newArray
+  }
 
+  const diceElements = dice.map(die => <Die value={die}/>)
+ 
 
+  return (
     <main>
       <div className='game-play'>
 
@@ -23,7 +27,7 @@ function App() {
         <p className='instructions'>Roll until all dice are the same. Click keach die to freeze it at its current value between rolls. Easy fun!</p> */}
 
           <div className='dice-container'>
-            <Die value="1"/>
+            {diceElements}
             
           </div>
 
