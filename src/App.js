d329af0c9a7ddf1ef9065c1b37a08704e219de2b
CB-Pantomime <christopherbrown.contact@gmail.com>
2022-06-03 14:19:21 -0700

    rendering dice container with 10 hard-coded die (for basic test) to page. dice container w/ basic styling display grid, in template of 5 columns of 2 rows. die-face basic styling and flex centered w/ box shadow.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 
 import './style.css';
+import Die from './Die'
 
 function App() {
   return (
     <main>
       <div className='game-play'>
-        <h1 className='title'>Tenzies</h1>
-        <p className='instructions'>Roll until all dice are the same. Click keach die to freeze it at its current value between rolls. Easy fun!</p>
+
+        {/* <h1 className='title'>Tenzies</h1>
+        <p className='instructions'>Roll until all dice are the same. Click keach die to freeze it at its current value between rolls. Easy fun!</p> */}
+
+          <div className='dice-container'>
+            <Die value="1"/>
+            <Die value="2"/>
+            <Die value="3"/>
+            <Die value="4"/>
+            <Die value="5"/>
+            <Die value="6"/>
+            <Die value="1"/>
+            <Die value="1"/>
+            <Die value="1"/>
+            <Die value="1"/>
+          </div>
+
       </div>
     </main>
   );
---
+++ src/Die.js
@@ -0,0 +1,12 @@
+
+import React from 'react'
+
+function Die (props) {
+    return (
+        <div className='die-face'>
+            <h2>{props.value}</h2>    
+        </div>
+    )
+}
+
+export default Die;
