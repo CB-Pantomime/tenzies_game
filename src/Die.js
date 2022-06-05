2dbd895b4dbfb1a4b3bc95770ead0de2b1bdd727
CB-Pantomime <christopherbrown.contact@gmail.com>
2022-06-05 11:29:32 -0700

    updated comments in Die component

--- src/Die.js
+++ src/Die.js
@@ -1,18 +1,26 @@
 
 import React from 'react'
 
+// our Die component receiving props
 function Die(props) {
 
+    // variable called styles referencing an object
+    // this object is a styles ternary 
+    // this will toggle die-face color from white to green if clicked to be held
     const styles = {
+       // backgroundColor is determined by isHeld is true or false
        backgroundColor: props.isHeld ? "#59E391" : "white"
     }
 
     return (
         <div 
         className='die-face' 
+        // style referencing our styles variable above
         style={styles}
+        // onClick referencing our handleClick passed through props
         onClick={props.handleClick}
         >
+            {/* passing value from dice state through props */}
             <h2 className='die-num'>{props.value}</h2>    
         </div>
     )
