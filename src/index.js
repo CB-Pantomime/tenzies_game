78fea6766f31ff0516b27ce0aa1272861b2b97a3
CB-Pantomime <christopherbrown.contact@gmail.com>
2022-06-05 10:23:27 -0700

    updated comments in index.js

--- src/index.js
+++ src/index.js
@@ -2,8 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-
+// This variable is referencing the react dom w/ the createRoot method
+// which is saying go to the document and get element by ID and that element is 'root'
 const root = ReactDOM.createRoot(document.getElementById('root'));
+// at that variable 'root', use the render method and render our App component
+// React.StrictMode is wrapped around it
+// this does: 
 root.render(
   <React.StrictMode>
     <App />
